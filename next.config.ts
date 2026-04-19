85d796517a7827b740883cb9f7482cfc137f2ff6
M-Jaya-Kumar-Patra <jayapatra2004@gmail.com>
2026-04-19 13:55:05 +0530

    cacheComponents

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // ppr: 'incremental', 
-    cacheComponents: true,
-  },
+  cacheComponents: true
 };
 
 export default nextConfig;
